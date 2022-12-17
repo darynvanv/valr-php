9fcaf87f52d8cae55294b4df2717d7864e17aa02
Daryn <daryn@ansio.co.za>
2022-12-17 10:15:13 +0200

    Initial Commit 2

---
+++ README.md
@@ -0,0 +1,5 @@
+VALR PHP
+===============
+
+[![Latest Version](https://img.shields.io/packagist/v/darynvanv/valr-php.svg?style=flat-square)](https://packagist.org/packages/darynvanv/valr-php)
+[![Total Downloads](https://img.shields.io/packagist/dt/darynvanv/valr-php.svg?style=flat-square)](https://packagist.org/packages/darynvanv/valr-php)
---
+++ src/VALR.php
@@ -0,0 +1,154 @@
+<?php
+namespace DarynvanV\VALR_PHP;
+
+use Exception;
+use Spatie\FlareClient\Http\Exceptions\NotFound;
+
+class VALR
+{
+    private $key;
+    private $secret;
+    
+    function __construct($key, $secret)
+    {
+        $this->key = $key;
+        $this->secret = $secret;
+    }
+
+    function get_balances()
+    {
+        $balances = $this->call_api('/v1/account/balances');
+        
+        return $balances;
+    }
+
+    function get_price($of, $to)
+    {
+        $price = $this->call_api('/v1/public/' . $of . $to . '/marketsummary');
+        return $price->lastTradedPrice;
+    }
+
+    function get_trades($of, $to, $limit = 100, $skip = 0, $before_id = null)
+    {
+        $trades = $this->call_api('/v1/public/' . $of . $to . '/trades?limit=' . $limit . '&skip=' . $skip . ($before_id ? '&beforeId=' . $before_id : ''));
+        return $trades;
+    }
+
+    function get_orders($of, $to)
+    {
+        $orders = $this->call_api('/v1/public/' . $of . $to . '/orderbook');
+        return $orders;
+    }
+
+    function get_currencies()
+    {
+        $currencies = $this->call_api('v1/public/currencies');
+        return $currencies;
+    }
+    function get_pairs()
+    {
+        $pairs = $this->call_api('v1/public/pairs');
+        return $pairs;
+    }
+
+    function sign($method, $path, $body = null, $timestamp = null)
+    {
+        $raw = $timestamp ?? (now()->timestamp * 1000);
+        $raw .= strtoupper($method);
+        $raw .= strtolower($path);
+        if($body)
+        {
+          $raw .= json_encode($body);
+        }
+
+        $algo = 'sha512';
+        
+        $signed_payload = hash_hmac(
+            $algo,
+            $raw,
+            $this->secret,
+            false,
+        );
+
+        return  $signed_payload;
+    }
+
+    function call_api($path, $method = "GET", $body = null)
+    {
+      if($path[0] != '/')
+        $path = '/' . $path;
+
+      if(substr($path, -1) == '/')
+        $path = substr($path, 0, strlen($path) - 1);
+
+      $curl = curl_init();
+
+      switch($method)
+      {
+        case "GET":
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.valr.com$path",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_CUSTOMREQUEST => $method,
+            CURLOPT_HTTPHEADER => array(
+              'X-VALR-API-KEY: ' . $this->key,
+              'X-VALR-SIGNATURE: ' . $this->sign($method,$path),
+              'X-VALR-TIMESTAMP: ' . now()->timestamp * 1000
+            ),
+          ));
+          break;
+        case "POST":
+          curl_setopt_array($curl, array(
+            CURLOPT_URL => "https://api.valr.com$path",
+            CURLOPT_RETURNTRANSFER => true,
+            CURLOPT_ENCODING => '',
+            CURLOPT_MAXREDIRS => 10,
+            CURLOPT_TIMEOUT => 0,
+            CURLOPT_FOLLOWLOCATION => true,
+            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+            CURLOPT_POST => 1,
+            CURLOPT_POSTFIELDS => $body,
+            CURLOPT_HTTPHEADER => array(
+              'X-VALR-API-KEY: ' . $this->key,
+              'X-VALR-SIGNATURE: ' . $this->sign($method,$path,$body),
+              'X-VALR-TIMESTAMP: ' . now()->timestamp * 1000
+            ),
+          ));
+          break;
+      }
+      
+
+      $response = curl_exec($curl);
+      
+      curl_close($curl);
+
+      $data = json_decode($response);
+
+      if(isset($data->code))
+      {
+        switch($data->code)
+        {
+          case -21:
+            throw new Exception("VALR: Currency pair not found.");
+            break;
+          case -93:
+            throw new Exception("VALR: Unauthorised.");
+            break;
+        }
+      }
+
+      if($data == null)
+      {
+        throw new Exception("VALR: Empty Response.");
+      }
+
+      dd($data);
+
+      return json_decode($response);
+    }
+}
